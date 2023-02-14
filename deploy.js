7452c3bf9adbe87bcb75e5f9dbf75057b03bbe21
Gaurav <79459872+gaurav-11018@users.noreply.github.com>
2023-02-14 20:45:06 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,13 +1,55 @@
-async function main() {
-  const verifierContract = "ERC20Verifier";
-  const verifierName = "ERC20zkAirdrop";
-  const verifierSymbol = "zkERC20";
-  const ERC20Verifier = await ethers.getContractFactory(verifierContract);
-  const erc20Verifier = await ERC20Verifier.deploy(
-    verifierName,
-    verifierSymbol
-  );
 
-  await erc20Verifier.deployed();
-  console.log(verifierName, " deployed to:", erc20Verifier.address);
+async function main() {
+
+    const circuitId = "credentialAtomicQuerySig";
+    const validatorAddress = "0xb1e86C4c687B85520eF4fd2a0d14e81970a15aFB";
+
+    // Grab the schema hash from Polygon ID Platform
+    const schemaHash = "<>"
+
+    const schemaEnd = fromLittleEndian(hexToBytes(schemaHash))
+    
+    const ageQuery = {
+    schema: ethers.BigNumber.from(schemaEnd),
+    slotIndex: 2,
+    operator: 2,
+    value: 20020101, ...new Array(63).fill(0).map(i => 0),
+    circuitId,
+    };
+
+    // add the address of the contract just deployed. An instance of the contract has already been deployed on Mumbai 0x752A8f2Fd1c5FC5c9241090BD183709D4591D4cb
+    ERC20VerifierAddress = "<>"
+
+    let erc20Verifier = await hre.ethers.getContractAt("ERC20Verifier", ERC20VerifierAddress)
+
+    const requestId = await erc20Verifier.TRANSFER_REQUEST_ID();
+
+    try {
+        await erc20Verifier.setZKPRequest(
+        requestId,
+        validatorAddress,
+        ageQuery
+        );
+        console.log("Request set");
+    } catch (e) {
+        console.log("error: ", e);
+    }
 }
+
+function hexToBytes(hex) {
+    for (var bytes = [], c = 0; c < hex.length; c += 2)
+        bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
+}
+
+function fromLittleEndian(bytes) {
+    const n256 = BigInt(256);
+    let result = BigInt(0);
+    let base = BigInt(1);
+    bytes.forEach((byte) => {
+      result += base * BigInt(byte);
+      base = base * n256;
+    });
+    return result;
+  }
+  
